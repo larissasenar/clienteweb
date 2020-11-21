99044f4fc357c3570db10e42fc67cf09fdef7c23
larissasena <Admin@SENAR-2895>
2020-11-21 12:35:07 -0300

    Estrutura inicial do projeto

---
+++ clienteweb/src/main/java/clienteweb/ClienteServlet.java
@@ -0,0 +1,48 @@
+package clienteweb;
+
+//Requests a Servlets pelo Método Post , Rest Client e GitHub
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns= {"/cliente" , "/clienteServlet" , "/clienteController" })
+
+public class ClienteServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		System.out.println("Chamou pelo método GET!");
+		
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		System.out.println("Chamou pelo método POST!");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método POST!");
+	}
+	
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Chamou pelo método DELETE!");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método DELETE!");
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Chamou pelo método PUT!");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método PUT!");
+	}
+}
